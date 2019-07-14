288d38187bcda746ad6f53ddec3041d220d25d17
Ewelina <e.k.urbas@gmail.com>
2019-07-14 16:58:32 +0200

    decreased number of Spotify requests, top 3 is now "max 3" not "3, unless there's a tie"

--- src/main/java/app/controller/TopGenresController.java
+++ src/main/java/app/controller/TopGenresController.java
@@ -12,15 +12,13 @@ public class TopGenresController {
     @Autowired
     TopGenresService topGenresService;
 
-    private boolean czyDupa = false;
     private String empty = "You have no followed artists, or your artists don't have genres added - there's really nothing I can show you :(";
     private String notAll = "You need more followed artists - this list would've been bigger!";
-
 
     @RequestMapping("/genres")
     public String getTopGenres(Model model) {
         model.addAttribute("genres", topGenresService.getTopGenres());
-        if (topGenresService.getTopGenres().size() == 0) {
+        if (topGenresService.getTopGenres().isEmpty()) {
             model.addAttribute("empty", empty);
         } else if (topGenresService.getTopGenres().size() < 3) {
             model.addAttribute("notAll", notAll);
